26f33b0256e671e7aab1385d164faee14fc0a200
viswa <viswa1126@gmail.com>
2021-06-04 15:26:55 +0530

    Created a Simple Component

---
+++ src/components/firstComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//ES6 new way of declaring components
+
+const Joe = () =>{
+    return <p>Joe is the first girl went to black hole..&#128540;</p>
+}
+
+// function Joe(){
+//     return <p>Joe is the first girl went to black hole..&#128540;</p>
+// }
+
+export default Joe;
