a16312a4279eec4dba7de4dfe88332fdd1aac8ca
viswa <viswa1126@gmail.com>
2021-06-04 15:59:24 +0530

    Added Dynamic Content

--- src/components/firstComponent.js
+++ src/components/firstComponent.js
@@ -3,7 +3,13 @@ import React from 'react';
 //ES6 new way of declaring components
 
 const Joe = () =>{
-    return <p>Joe is the first girl went to black hole..&#128540;</p>
+    return(
+    <div>
+            <p>Joe is the first girl went to black hole many times many years..&#128540;</p>
+            <p> Year {Math.floor(Math.random()*2000)}</p>
+
+    </div> 
+    )
 }
 
 // function Joe(){
